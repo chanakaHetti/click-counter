f901c1a7fbc8afbd3d772c8615f0a76c1a757b82
chanakacstv <chanaka@ceylonsolutions.com>
2020-09-23 11:26:02 +0530

    Unit testing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  // const [count, setCount] = React.useState(0);
+
+  render () {
+    return (
+      // <div data-test="component-app" className="App">
+      //   <h1 data-test="counter-display">
+      //     The counter is currently&nbsp; 
+      //     <span data-test="count">{count}</span>
+      //   </h1>
+      //   <button
+      //     data-test="increment-button"
+      //     onClick={() => setCount(count + 1)}
+      //   >
+      //     Increment counter
+      //   </button>
+      // </div>
+      <div data-test="component-app">
+        <h1>App</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
